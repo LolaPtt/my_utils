0e745826cc52907831ac496ac8291c6407d961f1
LolaPtt <67278953+LolaPtt@users.noreply.github.com>
2020-09-11 15:53:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<link href="style.css" rel="stylesheet" type="text/css" media="screen"/>
+
+</form>
+
+<table>
+	<thead>
+		<tr>
+			<th>N</th>
+			<th>Tasks</th>
+			<th style="width: 60px;">Action</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+		$tasks = mysqli_query($db, "SELECT * FROM tasks");
+
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="task"> <?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+</table>
+
---
+++ my_todo_list.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <title> My Todo List </title>
+
+</body>
+
+<h1> À faire </h1>
+<h2> Finie </h2>
+
+<ul> 
+    <input type="checkbox"> Faire les devoirs <br>
+    <input type="checkbox"> Sortir les chiens <br>
+    <input type="checkbox"> Faire la vaiselle <br>
+
+
+</ul>
+
+<ol>
+    <input type="submit" name="exple2" value="resultats"><a href="My_Todolist.html">
+
+</ol>
+
+</form>
+</html>
